3d21326cf286c806cc41147ff33bef92b875f370
limcpf <matathresh1740@naver.com>
2023-08-08 18:44:15 +0900

    - build 에러나는거 수정ㅠㅠ

--- components/Post/card/PostCard.tsx
+++ components/Post/card/PostCard.tsx
@@ -2,17 +2,12 @@
 
 import Post from "@/libs/class/Post.class";
 import PostCardContent from "@/components/Post/card/PostCardContent";
-import { useRouter } from "next/navigation";
 
 export default function PostCard({
   post,
 }: {
   post: Post;
 }) {
-  const router = useRouter();
-  // const goPost = () => router.push(`/post/${post.id}`);
-  const goPost = () => router.push(`/post/test`);
-
   return (
     <div
       className="
@@ -23,7 +18,7 @@ export default function PostCard({
           "
     >
       {/*<PostCardThumbnail url="https://files.limc.dev/file/limckr/test.png" goPost={goPost}/>*/}
-      <PostCardContent post={post} goPost={goPost} />
+      <PostCardContent post={post} />
       <div className="text-es text-gray-400 text-right font-extralight">
         {new Date(post.createdAt).toLocaleString()}
       </div>
