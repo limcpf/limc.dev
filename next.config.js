1a36681053cbfac371de6f042d928439040d9bed
limcpf <matathresh1740@naver.com>
2023-08-08 17:08:55 +0900

    - compress 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const API_SERVER_URL = process.env.NODE_ENV === "development" ? "http://localhost:8080" : process.env.API_SERVER_URL;
 
 const nextConfig = {
+  compress: true,
   images: {
     domains: ['files.limc.dev'],
   },
