3c7b3836ac38a11d6d4a16b9a653e056f6086d10
limcpf <matathresh1740@naver.com>
2023-08-11 23:50:33 +0900

    fix: admin.api 주소 수정

--- libs/api/Admin.api.ts
+++ libs/api/Admin.api.ts
@@ -1,10 +1,11 @@
 import {AdminDto} from "@/libs/dto/admin/AdminDto";
 import LoginDto from "@/libs/dto/admin/LoginDto";
+import {NEXT_PUBLIC_SERVER_URL} from "@/libs/constant/Api.constant";
 
 export async function login(adminDto:AdminDto) {
 
   const t = await fetch(
-      `https://api.limc.dev/public/login`, {
+      `${NEXT_PUBLIC_SERVER_URL}/api/public/login`, {
         method: "POST",
         body: JSON.stringify(adminDto),
         headers: new Headers({'content-type': 'application/json'}),
