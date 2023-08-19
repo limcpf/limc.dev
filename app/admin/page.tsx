8884723cbe0f9cf62ad3d1ca4731a750ce3beede
limcpf <matathresh1740@naver.com>
2023-08-19 16:16:53 +0900

    fix : useEffect 사용하여 시점 변경

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,29 +1,50 @@
 'use client';
+
 import PostPageBar from "@/components/Post/PostPageBar";
 import { getPostPageInAdmin } from "@/libs/api/Admin.api";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import React, {useState} from "react";
-import PostCard from "@/components/Post/card/PostCard";
+import React, {useEffect, useState} from "react";
+import PostCard from "@/components/Post/Card/PostCard";
 import Page from "@/libs/class/Page.class";
 import Post from "@/libs/class/Post.class";
+import AdminPostCard from "@/components/Admin/AdminPost/AdminPostCard/AdminPostCard";
+import {useRouter} from "next/navigation";
 
-export default  function adminMain(
+export default function adminMain(
     { searchParams } : {searchParams:Params}
 ) {
     const [postPage, setPostPage] = useState<Page<Post>>()
     const [content, setContent] = useState<Post[]>([])
+    const router = useRouter();
     const page = searchParams.page;
-    getPostPageInAdmin(page)
-        .then((page: Page<Post>) => {
-            setContent(page.content);
-            setPostPage(page)
-        })
-        .catch(e => console.log(e));
+
+    useEffect(() => {
+        getPostPageInAdmin(page)
+            .then((page: Page<Post>) => {
+                setContent(page.content);
+                setPostPage(page)
+            })
+            .catch(e => console.log(e));
+    }, [])
+
 
   return (
       <main className="w-full flex flex-col p-2">
+          <div className="flex w-full justify-end pb-2">
+              <div
+                  className="
+                    transition cursor-pointer
+                    bg-gray-300 text-gray-600
+                    hover:bg-gray-400 hover:text-gray-700
+                    px-5 py-1.5
+                  "
+                  onClick={() => router.push("/admin/post")}
+              >
+                  작성
+              </div>
+          </div>
           {content.length > 0 ? (
-              content.map((post) => <PostCard key={`post-${post.id}`} post={post} />)
+              content.map((post) => <AdminPostCard key={`post-${post.id}`} post={post} />)
           ) : (
               <div className={"h-52 flex justify-center items-center w-full "}>
                   게시글이 없습니다.
