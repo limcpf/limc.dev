827d1ae510df477f1c69bbdc51be5dca53736711
limcpf <matathresh1740@naver.com>
2023-08-19 16:16:03 +0900

    feat : admin layout 수정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import {cookies} from "next/headers";
 import AdminLogin from "@/components/Admin/AdminLogin";
+import AdminMenuBtn from "@/components/Admin/AdminMenuBtn";
+
+const adminMenu = [
+    {
+        href: "",
+        text: "Back"
+    },
+    {
+        href: "/admin",
+        text: "글"
+    },
+    {
+        href: "/admin/series",
+        text: "시리즈"
+    },
+    {
+        href: "/admin/topic",
+        text: "주제"
+    },
+    {
+        href: "/admin/site",
+        text: "사이트"
+    }
+]
 
 export default function AdminLayout({
     children
@@ -13,7 +37,16 @@ export default function AdminLayout({
   return (<>
     {
       !!token
-        ? <>{children}</>
+        ? <>
+          <div className="w-full gap-2  whitespace-nowrap flex px-4 py-3 border-b border-b-gray overflow-x-auto">
+              {
+                  adminMenu.map(
+                      ({href, text}) => <AdminMenuBtn href={href} text={text} />
+                  )
+              }
+          </div>
+              {children}
+          </>
         : (<AdminLogin  />)
     }
   </>);
