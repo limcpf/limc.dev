5813f9e0344beaa757e3dfa47d8b45c4c93784cc
limcpf <matathresh1740@naver.com>
2023-08-23 19:53:46 +0900

    fix: light 테마만 적용될 수 있도록 css import 수정

--- components/Post/PostWrapper.tsx
+++ components/Post/PostWrapper.tsx
@@ -1,4 +1,4 @@
-import "github-markdown-css";
+import "github-markdown-css/github-markdown-light.css";
 import Post from "@/libs/class/Post.class";
 import PostTitle from "@/components/Post/PostTitle";
 import PostContent from "@/components/Post/PostContent";
