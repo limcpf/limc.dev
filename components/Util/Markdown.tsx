e12e72db0017665d87ad88ab06ab2ebb084a176e
limcpf <matathresh1740@naver.com>
2023-09-04 22:51:14 +0900

    fix: video 설정(webm 지원)

--- components/Util/Markdown.tsx
+++ components/Util/Markdown.tsx
@@ -3,8 +3,8 @@ import "./Markdown.css";
 import Marked from "marked-react";
 import Image from "next/image";
 
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import {Light as SyntaxHighlighter} from "react-syntax-highlighter";
+import {darcula} from "react-syntax-highlighter/dist/esm/styles/hljs";
 import java from "react-syntax-highlighter/dist/esm/languages/hljs/java";
 import kotlin from "react-syntax-highlighter/dist/esm/languages/hljs/kotlin";
 import shell from "react-syntax-highlighter/dist/esm/languages/hljs/shell";
@@ -15,6 +15,7 @@ SyntaxHighlighter.registerLanguage("shell", shell);
 
 const renderer = {
   image(src: string, alt: string, title?: string | null) {
+    if(src.includes(".webm")) return <video key={alt} src={src} autoPlay loop />;
     return <Image key={alt} src={src} alt={alt} layout="fill" sizes="100%" />;
   },
   code(code: any, lang: string | undefined) {
